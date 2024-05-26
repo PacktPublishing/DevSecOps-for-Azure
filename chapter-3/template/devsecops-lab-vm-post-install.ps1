f515db51f367fa93cb694310b722792577959e56
David Okeyode <asegunlolu@hotmail.com>
2024-05-27 00:20:11 +0100

    Update devsecops-lab-vm-post-install.ps1

--- chapter-3/template/devsecops-lab-vm-post-install.ps1
+++ chapter-3/template/devsecops-lab-vm-post-install.ps1
@@ -1,7 +1,8 @@
 $env:chocolateyVersion = '1.4.0'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install git vscode azure-cli az.powershell nodejs.install dotnet-6.0-sdk dotnet-sdk dotnetcore-sdk googlechrome setdefaultbrowser win-no-annoy terraform netfx-4.8 -y
+choco install git vscode azure-cli az.powershell nodejs.install dotnet-6.0-sdk dotnet-sdk dotnetcore-sdk googlechrome setdefaultbrowser win-no-annoy terraform -y
+choco install netfx-4.8 -y
 choco install vcredist-all -y
 choco install miniconda3 --version=4.8.3 --params="'/AddToPath:1 /InstallationType:AllUsers /RegisterPython:1'" -y
 choco install microsoft-windows-terminal -y
